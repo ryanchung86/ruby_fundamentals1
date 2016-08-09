e36140dd9aa864c3b91c0f11c29e590a11fe2e5e
Ryan <ryan.y.chung@gmail.com>
2016-08-09 14:34:05 -0400

    completed exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,12 @@
+# 1 How would you calculate a good tip for a 55 dollar meal?
+price = 55
+puts price * 1.20
+
+# 2 Converting an integer into a string
+puts "hello " + 4.to_s
+
+# 3 Output using string interpolation
+puts "I am using string interpolation to print out the number #{45628 * 7839}"
+
+# 4 Comparison operator (pretty sure the output will be 'true')
+puts (10 < 20 && 30 < 20) || !(10 == 11)
