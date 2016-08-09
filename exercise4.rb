a6b63adaca940d42201e3ca45a135e43742d1d81
Ryan <ryan.y.chung@gmail.com>
2016-08-09 15:22:06 -0400

    completed exercise 4

---
+++ exercise4.rb
@@ -0,0 +1,14 @@
+# code taken from my prep work
+
+1.upto(100) { |num|
+if num % 3 == 0 && num % 5 == 0
+puts "BitMaker"
+    elsif num % 5 == 0
+puts "Maker"
+elsif
+num % 3 == 0
+    puts "Bit"
+else
+    puts num
+end
+}
